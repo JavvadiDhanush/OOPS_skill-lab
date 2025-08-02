0cb937cfb6bba338f45cd84d52228ce3b55cd77d
JavvadiDhanush <dhanushjavvadi@gmail.com>
2025-08-02 10:31:47 +0530

    Add files via upload

---
+++ EVENORODD.java
@@ -0,0 +1,21 @@
+package DHANUSH;
+import java.util.Scanner;
+
+public class EVENORODD {
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	int num = sc.nextInt();
+	
+	
+	if(num%2==0) {
+		
+		System.out.println("EVEN NUMBER");
+	} else {
+		System.out.println("ODD NUMBER");
+	}
+}
+
+	
+}
