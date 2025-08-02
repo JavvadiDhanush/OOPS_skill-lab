b6719191c9f66a802a87b9580d4a1a42d995a913
JavvadiDhanush <dhanushjavvadi@gmail.com>
2025-08-02 10:35:32 +0530

    Add files via upload

---
+++ temperature.java
@@ -0,0 +1,20 @@
+
+
+package DHANUSH;
+import java.util.Scanner;
+
+public class temperature {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = scanner.nextDouble();  
+
+        
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
+
+        scanner.close();
+    }
+}
