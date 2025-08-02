dc9e6ecb25748bc5425c7058a55bd7d23ccb6e81
JavvadiDhanush <dhanushjavvadi@gmail.com>
2025-08-02 10:35:47 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,16 @@
+package DHANUSH;
+import java.util.Scanner;
+public class While {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int i = sc.nextInt();
+		
+		while(i<=90) {
+			System.out.println(+i);
+			i++;
+		}
+	}
+}
